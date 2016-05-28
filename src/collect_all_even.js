3b543096c883a324132dacb6f3db4c21ef4c8a1e
Demo-tong <18829292377@163.com>
2016-05-28 16:54:42 +0800

    源代码，查找数组中偶数

---
+++ src/collect_all_even.js
@@ -0,0 +1,13 @@
+function collect_all_even(collection) {
+  var outArray = [];
+
+  for (var i = 0; i < collection.length; ++i) {
+    if (collection[i] % 2 === 0) {
+      outArray.push(collection[i]);
+    }
+  }
+
+  return outArray;
+}
+
+module.exports = collect_all_even;
